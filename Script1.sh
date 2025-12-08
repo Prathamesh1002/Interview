3622786e1e3e4c172cd8478661e2d6c3f580db0c
Prathamesh Hande <117574224+Prathamesh1002@users.noreply.github.com>
2025-12-08 08:45:49 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,6 +1,7 @@
-echo "I am from Young minds"
-echo "I am from batch-30 and learning DevOps"
 echo "My name is Prathamesh"
-echo "I am learning DevOps"
-echo "I am learning AWS"
+echo "I am learning AWS & DevOps"
 echo "I am learning Git/Github"
+Class=Young-minds
+Batch=25
+PROFESSION=AWS/DevSecOps/Azure DevOps
+echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
